4d87966a5f0f7ea9b1601a5f9394129e208d2fb1
ilyas <ilyasers@gmail.com>
2021-11-03 16:46:48 +0300

    style: change code to match linting rules

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { askName } from "./../src/cli.js";
+import console from 'console';
+import askName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-const name = askName();
+askName();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
+import console from 'console';
 import readlineSync from 'readline-sync';
 
-export const askName = () => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}`);
-    return name;
-}
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+  return name;
+};
