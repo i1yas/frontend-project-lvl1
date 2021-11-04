bbe4e5105f3c48d64c73b899c2d3290bf7402f70
ilyas <ilyasers@gmail.com>
2021-11-04 11:17:53 +0300

    refactor: move common games logic to index.js

--- src/games.js
+++ src/games.js
@@ -1,76 +1,61 @@
 import console from 'console';
-import readlineSync from 'readline-sync';
-import { getRandomInt } from './index.js';
+import { getRandomInt, askQuestions } from './index.js';
 
 export const startEvenGame = (name) => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  for (let i = 0; i < 3; i += 1) {
-    const number = getRandomInt(3, 100);
-    const isEven = number % 2 === 0;
-    const rightAnswer = isEven ? 'yes' : 'no';
-    console.log(`Question: ${number}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (rightAnswer === userAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`
-      );
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
+  askQuestions({
+    name,
+    getQuestion: () => {
+      const number = getRandomInt(3, 100);
+      const isEven = number % 2 === 0;
+      const answer = isEven ? 'yes' : 'no';
 
-  console.log(`Congratulations, ${name}!`);
-};
-
-const genCalcQuestion = () => {
-  const operators = ['+', '-', '/', '*'];
-  const op = operators[getRandomInt(0, operators.length - 1)];
-
-  const operands = [];
-  let answer;
-
-  if (op === '+') {
-    operands.push(getRandomInt(0, 100));
-    operands.push(getRandomInt(0, 100));
-    answer = operands[0] + operands[1];
-  }
-  if (op === '-') {
-    operands.push(getRandomInt(0, 100));
-    operands.push(getRandomInt(0, 100));
-    answer = operands[0] - operands[1];
-  }
-  if (op === '/') {
-    operands.push(getRandomInt(0, 50));
-    operands.push(getRandomInt(0, 10));
-    answer = operands[0] / operands[1];
-  }
-  if (op === '*') {
-    operands.push(getRandomInt(0, 20));
-    operands.push(getRandomInt(0, 10));
-    answer = operands[0] * operands[1];
-  }
-
-  return { operands, op, answer };
+      return { text: String(number), answer };
+    },
+    isEqual: (rightAnswer, userAnswer) => rightAnswer === userAnswer,
+  });
 };
 
 export const startCalcGame = (name) => {
   console.log('What is the result of the expression?');
 
-  for (let i = 0; i < 3; i += 1) {
-    const question = genCalcQuestion();
-    console.log(`Question: ${question.operands[0]} ${question.op} ${question.operands[1]}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (question.answer === Number(userAnswer)) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${question.answer}'.`);
-      console.log("Let's try again, Sam");
-      return;
-    }
-  }
+  askQuestions({
+    name,
+    getQuestion: () => {
+      let text;
+      let answer;
 
-  console.log(`Congratulations, ${name}`);
+      const operators = ['+', '-', '/', '*'];
+      const op = operators[getRandomInt(0, operators.length - 1)];
+
+      if (op === '+') {
+        const a = getRandomInt(0, 100);
+        const b = getRandomInt(0, 100);
+        answer = a + b;
+        text = `${a} + ${b}`;
+      }
+      if (op === '-') {
+        const a = getRandomInt(0, 100);
+        const b = getRandomInt(0, 100);
+        answer = a - b;
+        text = `${a} - ${b}`;
+      }
+      if (op === '/') {
+        const a = getRandomInt(0, 50);
+        const b = getRandomInt(0, 10);
+        answer = a / b;
+        text = `${a} / ${b}`;
+      }
+      if (op === '*') {
+        const a = getRandomInt(0, 20);
+        const b = getRandomInt(0, 10);
+        answer = a * b;
+        text = `${a} * ${b}`;
+      }
+
+      return { text, answer };
+    },
+    isEqual: (rightAnswer, userAnswer) => rightAnswer === Number(userAnswer),
+  });
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+import console from 'console';
+import readlineSync from 'readline-sync';
+
 export const getRandomInt = (min, max) => Math.round(min + Math.random() * (max - min));
+
+export const askQuestions = ({ name, getQuestion, isEqual }) => {
+  for (let i = 0; i < 3; i += 1) {
+    const question = getQuestion();
+    console.log(question.text);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (isEqual(question.answer, userAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${question.answer}'.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}`);
+};
